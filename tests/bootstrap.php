6c6c4b3937c6e2820b11e2ea2f9e4b63fa8f140c
Michael Dowling <mtdowling@gmail.com>
2011-04-21 08:49:53 -0500

    Fixing directory structure

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -5,4 +5,6 @@ namespace Cron\Tests;
 error_reporting(E_ALL | E_STRICT);
 
 require_once 'PHPUnit/TextUI/TestRunner.php';
-require_once __DIR__ . DIRECTORY_SEPARATOR . '..' . DIRECTORY_SEPARATOR . 'src' . DIRECTORY_SEPARATOR . 'CronExpression.php';
+require_once __DIR__ . DIRECTORY_SEPARATOR . '..' . DIRECTORY_SEPARATOR 
+    . 'src' . DIRECTORY_SEPARATOR . 'Cron' . DIRECTORY_SEPARATOR
+    . 'CronExpression.php';
