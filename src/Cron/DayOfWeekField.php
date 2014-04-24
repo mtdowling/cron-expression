4082f9774ae0c4a4ac906868af5a75dfc88b90e6
Viktoras Bezaras <vb@meets-ecommerce.de>
2014-04-24 21:17:05 +0000

    Fixed issue #20. Accept 0-7 MON-SUN for DayOfWeek field, also for hash notation

--- src/Cron/DayOfWeekField.php
+++ src/Cron/DayOfWeekField.php
@@ -45,9 +45,15 @@ class DayOfWeekField extends AbstractField
         // Handle # hash tokens
         if (strpos($value, '#')) {
             list($weekday, $nth) = explode('#', $value);
+
+            // 0 and 7 are both Sunday, however 7 matches date('N') format ISO-8601
+            if ($weekday === '0') {
+                $weekday = 7;
+            }
+
             // Validate the hash fields
-            if ($weekday < 1 || $weekday > 5) {
-                throw new \InvalidArgumentException("Weekday must be a value between 1 and 5. {$weekday} given");
+            if ($weekday < 0 || $weekday > 7) {
+                throw new \InvalidArgumentException("Weekday must be a value between 0 and 7. {$weekday} given");
             }
             if ($nth > 5) {
                 throw new \InvalidArgumentException('There are never more than 5 of a given weekday in a month');
