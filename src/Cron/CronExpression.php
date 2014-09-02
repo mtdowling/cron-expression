7a45c770afaa5408eeb74065c06363ce9e1b3811
latenzio <et@mandarin-medien.de>
2014-09-02 07:33:30 +0200

    Avoid annotation parsing by Doctrine
    fixes mtdowling/cron-expression#61

--- src/Cron/CronExpression.php
+++ src/Cron/CronExpression.php
@@ -45,11 +45,11 @@ class CronExpression
      *      several special predefined values which can be used to substitute the
      *      CRON expression:
      *
-     *      @yearly, @annually) - Run once a year, midnight, Jan. 1 - 0 0 1 1 *
-     *      @monthly - Run once a month, midnight, first of month - 0 0 1 * *
-     *      @weekly - Run once a week, midnight on Sun - 0 0 * * 0
-     *      @daily - Run once a day, midnight - 0 0 * * *
-     *      @hourly - Run once an hour, first minute - 0 * * * *
+     *      `@yearly`, `@annually` - Run once a year, midnight, Jan. 1 - 0 0 1 1 *
+     *      `@monthly` - Run once a month, midnight, first of month - 0 0 1 * *
+     *      `@weekly` - Run once a week, midnight on Sun - 0 0 * * 0
+     *      `@daily` - Run once a day, midnight - 0 0 * * *
+     *      `@hourly` - Run once an hour, first minute - 0 * * * *
      * @param FieldFactory $fieldFactory Field factory to use
      *
      * @return CronExpression
