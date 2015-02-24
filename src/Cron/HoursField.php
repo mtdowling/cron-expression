8bd59ca91a8be383c01e06ea0cdc6798270a6362
Michael Kahn <mkahn@intouchinsight.com>
2015-02-24 17:30:11 -0500

    cleaned up code formatting and removed unused var declaration

--- src/Cron/HoursField.php
+++ src/Cron/HoursField.php
@@ -7,38 +7,36 @@ namespace Cron;
  */
 class HoursField extends AbstractField
 {
-    public function isSatisfiedBy(\DateTime $date, $value)
+    public function isSatisfiedBy( \DateTime $date, $value )
     {
-        return $this->isSatisfied($date->format('H'), $value);
+        return $this->isSatisfied( $date->format( 'H' ), $value );
     }
 
-    public function increment(\DateTime $date, $invert = false)
+    public function increment( \DateTime $date, $invert = false )
     {
         // Change timezone to UTC temporarily. This will
         // allow us to go back or forwards and hour even
         // if DST will be changed between the hours.
-        $timezone = $date->getTimezone();
-		$localMinutes = $date->format('i');
-        $date->setTimezone(new \DateTimeZone('UTC'));
-		// handle timezones with non-hour-offsets	
-		$utcMinutes = $date->format('i');	
-		$minDiff = $localMinutes - $utcMinutes;
+        $timezone     = $date->getTimezone();
+        $localMinutes = $date->format( 'i' );
+        $date->setTimezone( new \DateTimeZone( 'UTC' ) );
+        // handle timezones with non-hour-offsets
+        $utcMinutes = $date->format( 'i' );
+        $minDiff    = $localMinutes - $utcMinutes;
         if ($invert) {
-            $date->modify('-1 hour');
-			$minutes = 59;
-            $date->setTime($date->format('H'), 59 - $minDiff);			
+            $date->modify( '-1 hour' );
+            $date->setTime( $date->format( 'H' ), 59 - $minDiff );
         } else {
-            $date->modify('+1 hour');
-			$minutes = 0;
-            $date->setTime($date->format('H'), 0 - $minDiff);		
+            $date->modify( '+1 hour' );
+            $date->setTime( $date->format( 'H' ), 0 - $minDiff );
         }
-        $date->setTimezone($timezone);
+        $date->setTimezone( $timezone );
 
         return $this;
     }
 
-    public function validate($value)
+    public function validate( $value )
     {
-        return (bool) preg_match('/^[\*,\/\-0-9]+$/', $value);
+        return (bool)preg_match( '/^[\*,\/\-0-9]+$/', $value );
     }
 }
