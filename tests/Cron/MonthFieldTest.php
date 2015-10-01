4a15016fd618a425ef7efff8c54dcc7d6e97b8fb
Alan Gervais <agervais@intouchinsight.com>
2015-10-01 10:14:37 -0400

    Removed code formatting that was added.

--- tests/Cron/MonthFieldTest.php
+++ tests/Cron/MonthFieldTest.php
@@ -16,10 +16,10 @@ class MonthFieldTest extends \PHPUnit_Framework_TestCase
     public function testValdatesField()
     {
         $f = new MonthField();
-        $this->assertTrue( $f->validate( '12' ) );
-        $this->assertTrue( $f->validate( '*' ) );
-        $this->assertTrue( $f->validate( '*/10,2,1-12' ) );
-        $this->assertFalse( $f->validate( '1.fix-regexp' ) );
+        $this->assertTrue($f->validate('12'));
+        $this->assertTrue($f->validate('*'));
+        $this->assertTrue($f->validate('*/10,2,1-12'));
+        $this->assertFalse($f->validate('1.fix-regexp'));
     }
 
     /**
@@ -27,14 +27,14 @@ class MonthFieldTest extends \PHPUnit_Framework_TestCase
      */
     public function testIncrementsDate()
     {
-        $d = new DateTime( '2011-03-15 11:15:00' );
+        $d = new DateTime('2011-03-15 11:15:00');
         $f = new MonthField();
-        $f->increment( $d );
-        $this->assertEquals( '2011-04-01 00:00:00', $d->format( 'Y-m-d H:i:s' ) );
+        $f->increment($d);
+        $this->assertEquals('2011-04-01 00:00:00', $d->format('Y-m-d H:i:s'));
 
-        $d = new DateTime( '2011-03-15 11:15:00' );
-        $f->increment( $d, true );
-        $this->assertEquals( '2011-02-28 23:59:00', $d->format( 'Y-m-d H:i:s' ) );
+        $d = new DateTime('2011-03-15 11:15:00');
+        $f->increment($d, true);
+        $this->assertEquals('2011-02-28 23:59:00', $d->format('Y-m-d H:i:s'));
     }
 
     /**
@@ -43,16 +43,16 @@ class MonthFieldTest extends \PHPUnit_Framework_TestCase
     public function testIncrementsDateWithThirtyMinuteTimezone()
     {
         $tz = date_default_timezone_get();
-        date_default_timezone_set( 'America/St_Johns' );
-        $d = new DateTime( '2011-03-31 11:59:59' );
+        date_default_timezone_set('America/St_Johns');
+        $d = new DateTime('2011-03-31 11:59:59');
         $f = new MonthField();
-        $f->increment( $d );
-        $this->assertEquals( '2011-04-01 00:00:00', $d->format( 'Y-m-d H:i:s' ) );
+        $f->increment($d);
+        $this->assertEquals('2011-04-01 00:00:00', $d->format('Y-m-d H:i:s'));
 
-        $d = new DateTime( '2011-03-15 11:15:00' );
-        $f->increment( $d, true );
-        $this->assertEquals( '2011-02-28 23:59:00', $d->format( 'Y-m-d H:i:s' ) );
-        date_default_timezone_set( $tz );
+        $d = new DateTime('2011-03-15 11:15:00');
+        $f->increment($d, true);
+        $this->assertEquals('2011-02-28 23:59:00', $d->format('Y-m-d H:i:s'));
+        date_default_timezone_set($tz);
     }
 
 
@@ -62,9 +62,9 @@ class MonthFieldTest extends \PHPUnit_Framework_TestCase
     public function testIncrementsYearAsNeeded()
     {
         $f = new MonthField();
-        $d = new DateTime( '2011-12-15 00:00:00' );
-        $f->increment( $d );
-        $this->assertEquals( '2012-01-01 00:00:00', $d->format( 'Y-m-d H:i:s' ) );
+        $d = new DateTime('2011-12-15 00:00:00');
+        $f->increment($d);
+        $this->assertEquals('2012-01-01 00:00:00', $d->format('Y-m-d H:i:s'));
     }
 
     /**
@@ -73,8 +73,8 @@ class MonthFieldTest extends \PHPUnit_Framework_TestCase
     public function testDecrementsYearAsNeeded()
     {
         $f = new MonthField();
-        $d = new DateTime( '2011-01-15 00:00:00' );
-        $f->increment( $d, true );
-        $this->assertEquals( '2010-12-31 23:59:00', $d->format( 'Y-m-d H:i:s' ) );
+        $d = new DateTime('2011-01-15 00:00:00');
+        $f->increment($d, true);
+        $this->assertEquals('2010-12-31 23:59:00', $d->format('Y-m-d H:i:s'));
     }
 }
