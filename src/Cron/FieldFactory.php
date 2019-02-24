5d2b25632a4f89f343efb21bb25bc7f70c60e763
Menno Holtkamp <menno.holtkamp@shopforce.nl>
2019-02-24 17:36:54 +0100

    Use null coalescing operator

--- src/Cron/FieldFactory.php
+++ src/Cron/FieldFactory.php
@@ -25,30 +25,26 @@ class FieldFactory
      */
     public function getField($position)
     {
-        if (!isset($this->fields[$position])) {
-            switch ($position) {
-                case 0:
-                    $this->fields[$position] = new MinutesField();
-                    break;
-                case 1:
-                    $this->fields[$position] = new HoursField();
-                    break;
-                case 2:
-                    $this->fields[$position] = new DayOfMonthField();
-                    break;
-                case 3:
-                    $this->fields[$position] = new MonthField();
-                    break;
-                case 4:
-                    $this->fields[$position] = new DayOfWeekField();
-                    break;
-                default:
-                    throw new InvalidArgumentException(
-                        ($position + 1) . ' is not a valid position'
-                    );
-            }
+        return $this->fields[$position] ?? $this->fields[$position] = $this->instantiateField($position);
+    }
+
+    private function instantiateField($position): FieldInterface
+    {
+        switch ($position) {
+            case CronExpression::MINUTE:
+                return new MinutesField();
+            case CronExpression::HOUR:
+                return new HoursField();
+            case CronExpression::DAY:
+                return new DayOfMonthField();
+            case CronExpression::MONTH:
+                return new MonthField();
+            case CronExpression::WEEKDAY:
+                return new DayOfWeekField();
         }
 
-        return $this->fields[$position];
+        throw new InvalidArgumentException(
+            ($position + 1) . ' is not a valid position'
+        );
     }
 }
